9d63fee056bb87103cdcaa66e4a132e3bcabee42
fellipenogueira <69211048+fellipenogueira@users.noreply.github.com>
2021-08-05 14:27:40 -0300

    Initial commit

---
+++ lib/entry.ts
@@ -0,0 +1,9 @@
+import {EntrypointParams} from '@vizir-banking/banking-app-core/dist/utils/entrypoint-params';
+
+export default ({
+  routesManager: _routesManager,
+  translationsManager: _translationsManager,
+  homeManager: _homeManager,
+  reduxManager: _reduxManager,
+  env: _env,
+}: EntrypointParams): void => {};
